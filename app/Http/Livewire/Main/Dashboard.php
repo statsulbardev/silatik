6d7700b12a4c7b452bf3615d1a1f6df1ce5cd3eb
Syaifur Rijal <syaifur.rijal@hotmail.com>
2022-08-02 10:40:00 +0800

    Hapus informasi pengembangan

--- app/Http/Livewire/Main/Dashboard.php
+++ app/Http/Livewire/Main/Dashboard.php
@@ -6,17 +6,6 @@ use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public $commit;
-    public $tag;
-    public $deskripsi;
-
-    public function mount()
-    {
-        $this->commit    = config('version.commit');
-        $this->tag       = config('version.tag');
-        $this->deskripsi = config('version.deskripsi');
-    }
-
     public function render()
     {
         return view('livewire.main.dashboard')->layout('layouts.main');
--- resources/views/livewire/main/dashboard.blade.php
+++ resources/views/livewire/main/dashboard.blade.php
@@ -34,26 +34,5 @@
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-12 col-md-12 col-lg-12">
-                <div class="card card-primary">
-                    <div class="card-header ui-sortable-handle d-flex justify-content-between">
-                        <h4>Versi Aplikasi : <i class="fas fa-tags"></i> {{ $tag }}</h4>
-                        <span class="font-weight-bold">
-                            Versi Commit : <i class="fas fa-code-commit"></i> {{ $commit }}
-                        </span>
-                    </div>
-                    <div class="card-body">
-                        <h4>Daftar Perbaikan dan Peningkatan :</h4>
-                        <ol>
-                            @foreach ($deskripsi as $item)
-                                <li>{{ $item }}</li>
-                            @endforeach
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
     </section>
 </div>
