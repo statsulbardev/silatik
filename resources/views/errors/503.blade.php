d65fca7707708ea1acc37862baefa18b22c8c05f
Syaifur Rijal <syaifur.rijal@hotmail.com>
2022-08-03 15:43:25 +0800

    Tambah Tampilan Error

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Pemeliharaan Website'))
+@section('code', '503')
+@section('message', __('Maaf, website ini sedang dalam pemeliharaan. Kami akan kembali beberapa saat lagi.'))
