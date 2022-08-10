f3c30d7ee7a9447bf5688d7cc70a124d91d7b066
Syaifur Rijal <syaifur.rijal@hotmail.com>
2022-08-10 20:32:42 +0800

    Pakai animasi loader

--- resources/views/components/partials/not-found.blade.php
+++ resources/views/components/partials/not-found.blade.php
@@ -1,9 +1,14 @@
 <div class="card-body">
-    <div class="empty-state" data-height="600" style="height: 600px;">
-        <img class="img-fluid" src="{{ secure_asset(env('APP_URL') . 'icons/not-found.svg') }}" alt="image">
-        <h2 class="mt-4">Surat yang anda cari tidak ditemukan.</h2>
+    <lottie-player class="mx-auto" src="https://assets7.lottiefiles.com/private_files/lf30_cgfdhxgx.json"  background="transparent"  speed="1"  style="width: 400px; height: 400px;"  loop  autoplay></lottie-player>
+
+    <div class="text-center">
+        <h4>Surat yang anda cari tidak ditemukan.</h4>
         <p class="lead">
             {{ $pesan }}
         </p>
     </div>
 </div>
+
+@push('scripts')
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+@endpush
