55386139919f6a45c7ab28769a858cf391538ccb
Syaifur Rijal <syaifur.rijal@hotmail.com>
2022-08-13 10:46:39 +0800

    Hapus berkas tidak terpakai

--- config/googleid.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'folder_root'  => env('GOOGLE_DRIVE_FOLDER_ROOT'),
-
-    'surat_masuk'  => env('GOOGLE_DRIVE_SURAT_MASUK'),
-
-    'surat_keluar' => env('GOOGLE_DRIVE_SURAT_KELUAR')
-];
